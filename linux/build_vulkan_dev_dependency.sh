15df51887b6d54ffe7653bc776be69cfeabb89e5
QSXW <47172719+QSXW@users.noreply.github.com>
2021-11-22 14:06:42 +0800

    Update build_vulkan_dev_dependency.sh

--- linux/build_vulkan_dev_dependency.sh
+++ linux/build_vulkan_dev_dependency.sh
@@ -35,6 +35,11 @@ EOF
     sudo mv "$BUILD_PREFIX"/vulkan.pc /usr/lib/pkgconfig/
 
     cd ..
+    
+    wget -qO - http://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+    sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-bionic.list http://packages.lunarg.com/vulkan/lunarg-vulkan-bionic.list
+    sudo apt update
+    sudo apt install vulkan-sdk
 }
 
 build_glslang_dependency()
